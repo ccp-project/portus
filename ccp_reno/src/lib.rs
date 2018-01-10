5fe6ba2ef60587ebb82e13087404c8070dae7bfd
Akshay Narayan <akshayn@mit.edu>
2018-01-09 23:02:52 -0500

    reno: keep track of cwnd_deficit
    We implement reno carefully to avoid cutting the window too much.
    
    1. On a report of loss, cut the cwnd in half, and set cwnd_deficit to
    the number of lost + sacked packets.
    2. If more sacks or losses arrive without a full or partial ACK, DO NOT
    cut the window further, but rather increment the cwnd_deficit.
    3. If a partial ACK arrives, decrement cwnd_deficit by the amount of the
    ACK. IFF cwnd == ss_thresh, then assume the connection was probing for
    bandwidth before the loss, and halve the window.
    4. If a full ACK arrives, decrement cwnd_deficit by the ACK amount.
    
    Once cwnd_deficit reaches zero, then further losses are once again free
    to affect the window.

--- ccp_reno/src/lib.rs
+++ ccp_reno/src/lib.rs
@@ -2,9 +2,7 @@
 extern crate slog;
 #[macro_use]
 extern crate portus;
-extern crate time;
 
-use time::Timespec;
 use portus::{CongAlg, Datapath, Measurement};
 use portus::pattern;
 use portus::ipc::Ipc;
@@ -17,7 +15,7 @@ pub struct Reno<T: Ipc> {
     sock_id: u32,
     ss_thresh: u32,
     cwnd: u32,
-    last_cwnd_reduction: Timespec,
+    curr_cwnd_reduction: u32,
     init_cwnd: u32,
 }
 
@@ -44,10 +42,11 @@ impl<T: Ipc> Reno<T> {
         match self.control_channel.install_measurement(
             self.sock_id,
             "
-                (def (acked 0) (loss 0) (timeout false) (rtt 0) (inflight 0))
+                (def (acked 0) (sacked 0) (loss 0) (timeout false) (rtt 0) (inflight 0))
                 (bind Flow.inflight Pkt.packets_in_flight)
                 (bind Flow.rtt Pkt.rtt_sample_us)
                 (bind Flow.acked (+ Flow.acked Pkt.bytes_acked))
+                (bind Flow.sacked (+ Flow.sacked Pkt.packets_misordered))
                 (bind Flow.loss Pkt.lost_pkts_sample)
                 (bind Flow.timeout Pkt.was_timeout)
                 (bind isUrgent Pkt.was_timeout)
@@ -60,14 +59,14 @@ impl<T: Ipc> Reno<T> {
         }
     }
 
-    fn get_fields(&mut self, m: Measurement) -> (u32, bool, bool, u32, u32, u32) {
+    fn get_fields(&mut self, m: Measurement) -> (u32, bool, u32, u32, u32, u32) {
         let sc = self.sc.as_ref().expect("scope should be initialized");
         let ack = m.get_field(&String::from("Flow.acked"), sc).expect(
-            "expected ack field in returned measurement",
+            "expected acked field in returned measurement",
         ) as u32;
-
-        let was_urgent = m.get_field(&String::from("isUrgent"), sc).expect(
-            "expected isUrgent field in returned measurement",
+        
+        let sack = m.get_field(&String::from("Flow.sacked"), sc).expect(
+            "expected sacked field in returned measurement",
         ) as u32;
 
         let was_timeout = m.get_field(&String::from("Flow.timeout"), sc).expect(
@@ -86,48 +85,66 @@ impl<T: Ipc> Reno<T> {
             "expected rtt field in returned measurement",
         ) as u32;
 
-        (ack, was_urgent == 1, was_timeout == 1, loss, rtt, inflight)
+        (ack, was_timeout == 1, sack, loss, rtt, inflight)
     }
 
-    fn handle_urgent(&mut self, was_timeout: bool, loss: u32, rtt_us: u32) {
-        if time::get_time() - self.last_cwnd_reduction <
-            time::Duration::nanoseconds((rtt_us as u64 * 1000) as i64)
-        {
-            return;
+    fn additive_increase_with_slow_start(&mut self, acked: u32) {
+        let mut new_bytes_acked = acked;
+        if self.cwnd < self.ss_thresh {
+            // increase cwnd by 1 per packet, until ssthresh
+            if self.cwnd + new_bytes_acked > self.ss_thresh {
+                new_bytes_acked -= self.ss_thresh - self.cwnd;
+                self.cwnd = self.ss_thresh;
+            } else {
+                self.cwnd += new_bytes_acked;
+                new_bytes_acked = 0;
+            }
         }
 
-        self.last_cwnd_reduction = time::get_time();
+        // increase cwnd by 1 / cwnd per packet
+        self.cwnd += 1448u32 * (new_bytes_acked / self.cwnd);
+    }
 
-        if was_timeout {
-            self.ss_thresh /= 2;
-            if self.ss_thresh < self.init_cwnd {
-                self.ss_thresh = self.init_cwnd;
-            }
+    fn handle_timeout(&mut self) {
+        self.ss_thresh /= 2;
+        if self.ss_thresh < self.init_cwnd {
+            self.ss_thresh = self.init_cwnd;
+        }
 
-            self.cwnd = self.init_cwnd;
+        self.cwnd = self.init_cwnd;
+        self.curr_cwnd_reduction = 0;
 
-            self.logger.as_ref().map(|log| {
-                warn!(log, "timeout"; 
-                    "curr_cwnd (pkts)" => self.cwnd / 1460, 
-                    "ssthresh" => self.ss_thresh,
-                    "loss" => loss,
-                );
-            });
-        } else if loss > 0 {
-            // else, treat as isolated loss
+        self.logger.as_ref().map(|log| {
+            warn!(log, "timeout"; 
+                "curr_cwnd (pkts)" => self.cwnd / 1448, 
+                "ssthresh" => self.ss_thresh,
+            );
+        });
+
+        self.send_pattern();
+        return;
+    }
+
+    /// Handle sacked or lost packets
+    /// Only call with loss > 0 || sacked > 0
+    fn cwnd_reduction(&mut self, loss: u32, sacked: u32, acked: u32) {
+        // if loss indicator is nonzero  
+        // AND the losses in the lossy cwnd have not yet been accounted for
+        // OR there is a partial ACK AND cwnd was probing ss_thresh
+        if loss > 0 && self.curr_cwnd_reduction == 0 || (acked > 0 && self.cwnd == self.ss_thresh) {
             self.cwnd /= 2;
             if self.cwnd <= self.init_cwnd {
                 self.cwnd = self.init_cwnd;
             }
 
             self.ss_thresh = self.cwnd;
-
-            self.logger.as_ref().map(|log| {
-                info!(log, "urgent"; "curr_cwnd (pkts)" => self.cwnd / 1460, "loss" => loss);
-            });
+            self.send_pattern();
         }
-
-        self.send_pattern();
+        
+        self.curr_cwnd_reduction += sacked + loss;
+        self.logger.as_ref().map(|log| {
+            info!(log, "loss"; "curr_cwnd (pkts)" => self.cwnd / 1448, "loss" => loss, "sacked" => sacked, "curr_cwnd_deficit" => self.curr_cwnd_reduction);
+        });
     }
 }
 
@@ -147,7 +164,7 @@ impl<T: Ipc> CongAlg<T> for Reno<T> {
             logger: log_opt,
             cwnd: init_cwnd,
             init_cwnd: init_cwnd,
-            last_cwnd_reduction: time::get_time(),
+            curr_cwnd_reduction: 0,
             sc: None,
             sock_id: sock_id,
             ss_thresh: 0x7fffffff,
@@ -163,35 +180,40 @@ impl<T: Ipc> CongAlg<T> for Reno<T> {
     }
 
     fn measurement(&mut self, _sock_id: u32, m: Measurement) {
-        let (acked, was_urgent, was_timeout, loss, rtt, inflight) = self.get_fields(m);
-        if was_urgent {
-            self.handle_urgent(was_timeout, loss, rtt);
+        let (acked, was_timeout, sacked, loss, rtt, inflight) = self.get_fields(m);
+        if was_timeout {
+            self.handle_timeout();
             return;
         }
 
-        let mut new_bytes_acked = acked;
-        if self.cwnd < self.ss_thresh {
-            // increase cwnd by 1 per packet, until ssthresh
-            if self.cwnd + new_bytes_acked > self.ss_thresh {
-                new_bytes_acked -= self.ss_thresh - self.cwnd;
-                self.cwnd = self.ss_thresh;
-            } else {
-                self.cwnd += new_bytes_acked;
-                new_bytes_acked = 0;
-            }
+        // increase the cwnd corresponding to new in-order cumulative ACKs
+        self.additive_increase_with_slow_start(acked);
+
+        if loss > 0 || sacked > 0 {
+            self.cwnd_reduction(loss, sacked, acked);
+        } else if acked < self.curr_cwnd_reduction {
+            self.curr_cwnd_reduction -= acked / 1448u32;
+        } else {
+            self.curr_cwnd_reduction = 0;
         }
 
-        // increase cwnd by 1 / cwnd per packet
-        self.cwnd += 1460u32 * (new_bytes_acked / self.cwnd);
+        if self.curr_cwnd_reduction > 0 {
+            self.logger.as_ref().map(|log| {
+                debug!(log, "in cwnd reduction"; "acked" => acked / 1448u32, "deficit" => self.curr_cwnd_reduction);
+            });
+            return;
+        }
+
         self.send_pattern();
 
         self.logger.as_ref().map(|log| {
             debug!(log, "got ack"; 
-                "acked(bytes)" => acked, 
+                "acked(pkts)" => acked / 1448u32, 
                 "curr_cwnd (pkts)" => self.cwnd / 1460, 
                 "inflight (pkts)" => inflight, 
                 "loss" => loss, 
-                "ssthresh" => self.ss_thresh
+                "ssthresh" => self.ss_thresh,
+                "rtt" => rtt,
             );
         });
     }
