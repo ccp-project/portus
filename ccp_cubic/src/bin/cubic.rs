8eab7dafea11143b19584fb2da05b492bc396048
prateesh <prateesh@github.mit.edu>
2018-01-15 16:44:03 -0500

    ccp_cubic: implementation based on ccp_reno

---
+++ ccp_cubic/src/bin/cubic.rs
@@ -0,0 +1,119 @@
+extern crate clap;
+
+#[macro_use]
+extern crate slog;
+extern crate slog_term;
+extern crate slog_async;
+use slog::Drain;
+
+extern crate ccp_cubic;
+extern crate portus;
+
+use clap::Arg;
+use ccp_cubic::Cubic;
+use portus::ipc::Backend;
+
+fn make_logger() -> slog::Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    slog::Logger::root(drain, o!())
+}
+
+fn make_args() -> Result<(ccp_cubic::CubicConfig, String), std::num::ParseFloatError> {
+    let ss_thresh_default = format!("{}", ccp_cubic::DEFAULT_SS_THRESH);
+    let matches = clap::App::new("CCP Cubic")
+        .version("0.1.0")
+        .author("Prateesh Goyal <prateesh@mit.edu>")
+        .about("Implementation of Cubic Congestion Control")
+        .arg(Arg::with_name("ipc")
+             .long("ipc")
+             .help("Sets the type of ipc to use: (netlink|unix)")
+             .default_value("unix")
+             .validator(portus::ipc_valid))
+        .arg(Arg::with_name("ss_thresh")
+             .long("ss_thresh")
+             .help("Sets the slow start threshold, in bytes")
+             .default_value(&ss_thresh_default))
+        .arg(Arg::with_name("init_cwnd")
+             .long("init_cwnd")
+             .help("Sets the initial congestion window, in bytes. Setting 0 will use datapath default.")
+             .default_value("10"))
+        .get_matches();
+
+    Ok((
+        ccp_cubic::CubicConfig {
+            ss_thresh: matches.value_of("ss_thresh").unwrap().parse()?,
+            init_cwnd: matches.value_of("init_cwnd").unwrap().parse()?,
+        },
+        String::from(matches.value_of("ipc").unwrap()),
+    ))
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() {
+    let log = make_logger();
+    let (cfg, ipc) = make_args()
+        .map_err(|e| warn!(log, "bad argument"; "err" => ?e))
+        .unwrap_or(Default::default());
+
+    info!(log, "starting CCP Cubic");
+    match ipc.as_str() {
+        "unix" => {
+            use portus::ipc::unix::Socket;
+            let b = Socket::new(0).and_then(|sk| Backend::new(sk)).expect(
+                "ipc initialization",
+            );
+
+            portus::start::<_, Cubic<Socket>>(
+                b,
+                portus::Config {
+                    logger: Some(log),
+                    config: cfg,
+                },
+            );
+        }
+        _ => unreachable!(),
+    }
+}
+
+#[cfg(all(target_os = "linux"))]
+fn main() {
+    let log = make_logger();
+    let (cfg, ipc) = make_args()
+        .map_err(|e| warn!(log, "bad argument"; "err" => ?e))
+        .unwrap_or(Default::default());
+
+    info!(log, "starting CCP Cubic");
+    match ipc.as_str() {
+        "unix" => {
+            use portus::ipc::unix::Socket;
+            let b = Socket::new(0).and_then(|sk| Backend::new(sk)).expect(
+                "ipc initialization",
+            );
+
+            portus::start::<_, Cubic<Socket>>(
+                b,
+                portus::Config {
+                    logger: Some(log),
+                    config: cfg,
+                },
+            );
+        }
+        "netlink" => {
+            use portus::ipc::netlink::Socket;
+            let b = Socket::new().and_then(|sk| Backend::new(sk)).expect(
+                "ipc initialization",
+            );
+
+            portus::start::<_, Cubic<Socket>>(
+                b,
+                portus::Config {
+                    logger: Some(log),
+                    config: cfg,
+                },
+            );
+        }
+        _ => unreachable!(),
+    }
+}
