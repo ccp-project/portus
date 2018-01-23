038948933bd1980a253f5291f3a570e09c13b46b
Srinivas Narayana <ngsrinivas@gmail.com>
2018-01-23 17:59:24 +0000

    Fix socket initialization compile error

--- ccp_aggregation_example/src/bin/aggregator.rs
+++ ccp_aggregation_example/src/bin/aggregator.rs
@@ -49,7 +49,7 @@ fn main() {
     match ipc.as_str() {
         "unix" => {
             use portus::ipc::unix::Socket;
-            let b = Socket::new(0).and_then(|sk| Backend::new(sk)).expect(
+            let b = Socket::new("in", "out").and_then(|sk| Backend::new(sk)).expect(
                 "ipc initialization",
             );
 
@@ -76,7 +76,7 @@ fn main() {
     match ipc.as_str() {
         "unix" => {
             use portus::ipc::unix::Socket;
-            let b = Socket::new(0).and_then(|sk| Backend::new(sk)).expect(
+            let b = Socket::new("in", "out").and_then(|sk| Backend::new(sk)).expect(
                 "ipc initialization",
             );
 
