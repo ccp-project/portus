5da54bed589eb12f7105e530dc91cdc00183736a
Akshay Narayan <akshayn@mit.edu>
2018-03-15 13:48:10 -0400

    kp: update test to compile on new kernels
    Replace <asm/uaccess.h> with <linux/uaccess.h>.

--- src/ipc/test-char-dev/ccpkp.c
+++ src/ipc/test-char-dev/ccpkp.c
@@ -6,21 +6,19 @@
  *
  */
 
+#include <linux/atomic.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/errno.h>
+#include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/device.h>
-
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#include <linux/slab.h>
-#include <linux/errno.h>
-#include <linux/types.h>
-#include <linux/cdev.h>
 #include <linux/mutex.h>
-#include <linux/atomic.h>
-
-#include <asm/uaccess.h>
+#include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
 
 #include "ccpkp.h"
 
