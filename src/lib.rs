560772cc82ca76ec4281131233aa6c667c804348
Frank Cangialosi <frankc@csail.mit.edu>
2018-05-25 23:46:18 -0400

    aggregation: add new api for aggregate cc across flows

--- src/lib.rs
+++ src/lib.rs
@@ -206,7 +206,7 @@ where
     }
 }
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 /// The set of information passed by the datapath to CCP
 /// when a connection starts. It includes a unique 5-tuple (CCP socket id + source and destination
 /// IP and port), the initial congestion window (`init_cwnd`), and flow MSS.
@@ -263,6 +263,30 @@ pub trait CongAlg<T: Ipc> {
     fn close(&mut self) {} // default implementation does nothing (optional method)
 }
 
+/// Implement this trait (and [`CongAlg`](./trait.CongAlg.html)) to define an algorithm that performs aggregate congestion control across multiple flows.
+/// An instance of a struct implementing this trait represents an aggregate bundle of flows.
+///
+/// The internal [`Key`](./trait.Aggregator.html#associatedtype.Key) type determines which flows belong to this bundle. 
+/// 1. [`CongAlg::create`](./trait.CongAlg.html#tymethod.create) is called when the first flow matching this key is started.
+/// 2. [`new_flow`](./trait.Aggregator.html#tymethod.new_flow) is called for each additional flow that starts and joins the bundle.
+/// 3. [`close_one`](./trait.Aggregator.html#tymethod.close_one) is called each time a flow finishes and leaves the bundles. 
+/// 4. [`CongAlg::close`](./trait.CongAlg.html#method.close) is called when there are no longer any flows belonging to this bundle.
+/// Immediately after this call the struct will be destroyed. 
+pub trait Aggregator<T: Ipc> {
+    /// Aggregators provide this type to define how flows are binned into aggregates.
+    /// This key must implement the equality, hash, debug, and copy traits,
+    /// and is as a function of the corresponding flow's DatapathInfo struct.
+    type Key: From<DatapathInfo> + std::cmp::Eq + std::hash::Hash + std::fmt::Debug + Copy;
+
+    /// If a new flow corresponds to an existing aggregate, replace the create() method
+    /// from CongAlg with new_flow() to notify the aggregate of a new flow arrival.
+    fn new_flow(&mut self, info: DatapathInfo, control: Datapath<T>);
+
+    /// Called when a flow belonging to this aggregate ends.
+    fn close_one(&mut self, key: &Self::Key);
+
+}
+
 #[derive(Debug)]
 /// A handle to manage running instances of the CCP execution loop.
 pub struct CCPHandle {
@@ -314,6 +338,20 @@ where
     }
 }
 
+/// Aggregate congestion control version of [`run`](./fn.run.html).
+/// 
+/// Same as [`run`](./fn.run.html), except `U` must also implement the [`Aggregator<I>`](./trait.Aggregator.html) trait in addition to [`CongAlg<I>`](./trait.CongAlg.html).
+pub fn run_aggregator<I, U>(backend_builder: BackendBuilder<I>, cfg: &Config<I, U>) -> Result<!>
+where
+    I: Ipc,
+    U: CongAlg<I> + Aggregator<I>,
+{
+    match run_aggregator_inner(backend_builder, cfg, Arc::new(atomic::AtomicBool::new(true))) {
+        Ok(_) => unreachable!(),
+        Err(e) => Err(e),
+    }
+}
+
 /// Spawn a thread which will perform the CCP execution loop. Returns
 /// a `CCPHandle`, which the caller can use to cause the execution loop
 /// to stop.
@@ -334,6 +372,24 @@ where
         join_handle: thread::spawn(move || {
             run_inner(backend_builder, &cfg, stop_signal.clone())
         }),
+    }
+}
+
+/// Aggregate congestion control version of [`spawn`](./fn.spawn.html).
+///
+/// Same as [`spawn`](./fn.spawn.html), except `U` must also implement the [`Aggregator<I>`](./trait.Aggregator.html) trait in addition to [`CongAlg<I>`](./trait.CongAlg.html).
+
+pub fn spawn_aggregator<I, U>(backend_builder: BackendBuilder<I>, cfg: Config<I, U>) -> CCPHandle
+where
+    I: Ipc,
+    U: CongAlg<I> + Aggregator<I>,
+{
+    let stop_signal = Arc::new(atomic::AtomicBool::new(true));
+    CCPHandle {
+        continue_listening: stop_signal.clone(),
+        join_handle: thread::spawn(move || {
+            run_inner(backend_builder, &cfg, stop_signal.clone())
+        })
     }
 }
 
@@ -437,5 +493,130 @@ where
         Err(Error(String::from("The IPC channel has closed.")))
     }
 }
+
+// Aggregate congestion control version of main inner execution loop of CCP
+// Blocks "forever", or until the iterator stops iterator. 
+// 
+// Maps flow ids to keys (as defined by user-provided Aggregator). Only a single instance of `U` is created for each aggregate.
+// When a new flow has a key that we have not yet seen before, we create an instance of `U` (this only uses the `CongAlg` trait)
+// When a new flow has a key that has already been mapped, we retrieve the instance of `U` and call `U::new_flow`
+// See [`run_inner`](./fn.run_inner.html) for more details about return codes. 
+fn run_aggregator_inner<I, U>(backend_builder: BackendBuilder<I>, cfg: &Config<I, U>, continue_listening: Arc<atomic::AtomicBool>) -> Result<()>
+where
+    I: Ipc,
+    U: CongAlg<I> + Aggregator<I>,
+{
+    let mut receive_buf = [0u8; 1024];
+    let mut  b = backend_builder.build(continue_listening.clone(), &mut receive_buf[..]);
+    let mut flows = HashMap::<u32, U::Key>::new();
+    let mut aggregates = HashMap::<U::Key, U>::new();
+    let mut num_flows_per_agg = HashMap::<U::Key, u32>::new();
+    let backend = b.sender();
+    while let Some(msg) = b.next() {
+        match msg {
+            Msg::Cr(c) => {
+                if flows.remove(&c.sid).is_some() {
+                    cfg.logger.as_ref().map(|log| {
+                        debug!(log, "re-creating already created flow"; "sid" => c.sid);
+                    });
+                }
+
+                cfg.logger.as_ref().map(|log| {
+                    debug!(log, "creating new flow"; 
+                           "sid" => c.sid, 
+                           "init_cwnd" => c.init_cwnd,
+                           "mss"  =>  c.mss,
+                           "src_ip"  =>  c.src_ip,
+                           "src_port"  =>  c.src_port,
+                           "dst_ip"  =>  c.dst_ip,
+                           "dst_port"  =>  c.dst_port,
+                    );
+                });
+
+                let info = DatapathInfo {
+                        sock_id: c.sid,
+                        init_cwnd: c.init_cwnd,
+                        mss: c.mss,
+                        src_ip: c.src_ip,
+                        src_port: c.src_port,
+                        dst_ip: c.dst_ip,
+                        dst_port: c.dst_port,
+                };
+
+                let k = U::Key::from(info);
+
+                aggregates.get_mut(&k).and_then(|agg| {
+                    agg.new_flow(info, Datapath {
+                        sock_id: c.sid,
+                        sender: backend.clone(),
+                    });
+                    Some(())
+                }).or_else(|| {
+                    let agg = U::create(
+                        Datapath{
+                            sock_id: c.sid,
+                            sender: backend.clone(),
+                        },
+                        cfg.clone(),
+                        info
+                    );
+                    aggregates.insert(k, agg);
+                    Some(())
+                });
+
+                flows.insert(c.sid, k);
+                *num_flows_per_agg.entry(k).or_insert(0) += 1;
+
+            }
+            Msg::Ms(m) => {
+                if flows.contains_key(&m.sid) {
+                    if m.num_fields == 0 {
+                        let mut key = flows.remove(&m.sid).unwrap();
+                        if aggregates.contains_key(&key) {
+                            let num_flows = num_flows_per_agg.get_mut(&key).unwrap();
+                            *num_flows -= 1;
+                            if *num_flows == 0 {
+                                aggregates.remove(&key).unwrap();
+                            }
+                            aggregates.get_mut(&key).and_then(|agg| {
+                                agg.close_one(&key);
+                                if *num_flows == 0 {
+                                    agg.close();
+                                }
+                                Some(())
+                            });
+                        } else {
+                            eprintln!("error: unknown aggregate key {:?}!", key);
+                        }
+                    } else {
+                        let mut key = flows.get_mut(&m.sid).unwrap();
+                        aggregates.get_mut(&key).and_then(move |agg| {
+                            agg.on_report(m.sid, Report { fields: m.fields });
+                            Some(())
+                        }).or_else(|| {
+                            eprintln!("error: unknown aggregate key {:?}!", key);
+                            Some(())
+                        });
+                    }
+                } else {
+                    cfg.logger.as_ref().map(|log| {
+                        debug!(log, "measurement for unknown flow"; "sid" => m.sid);
+                    });
+                }
+            }
+            Msg::Ins(_) => {
+                unimplemented!()
+            }
+            _ => continue,
+        }
+    }
+    // if the thread has been killed, return that as error
+    if !continue_listening.load(atomic::Ordering::SeqCst) {
+        Ok(())
+    } else {
+        Err(Error(String::from("The IPC channel has closed.")))
+    }
+}
+
 #[cfg(test)]
 mod test;
