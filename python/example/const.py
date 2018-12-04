0bd1690082ef8b6c833c8cdb797e6657568fc43d
Frank Cangialosi <frankc@csail.mit.edu>
2018-12-04 02:16:45 +0000

    python: add const rate example alg

---
+++ python/example/const.py
@@ -0,0 +1,41 @@
+import sys
+import portus
+
+class ConstFlow():
+    INIT_RATE = 1000000
+
+    def __init__(self, datapath, datapath_info):
+        self.datapath = datapath
+        self.datapath_info = datapath_info
+        self.rate = ConstFlow.INIT_RATE
+        self.datapath.set_program("default", [("Rate", self.rate)])
+
+    def on_report(self, r):
+        self.datapath.update_field("Rate", self.rate)
+
+
+class Const(portus.AlgBase):
+
+    def datapath_programs(self):
+        return {
+                "default" : """\
+                (def (Report
+                    (volatile acked 0) 
+                    (volatile loss 0) 
+                    (volatile rtt 0)
+                ))
+                (when true
+                    (:= Report.rtt Flow.rtt_sample_us)
+                    (:= Report.acked (+ Report.acked Ack.bytes_acked))
+                    (:= Report.loss Ack.lost_pkts_sample)
+                    (report)
+                )
+            """
+        }
+
+    def new_flow(self, datapath, datapath_info):
+        return ConstFlow(datapath, datapath_info)
+
+alg = Const()
+
+portus.start("unix", alg, debug=True)
